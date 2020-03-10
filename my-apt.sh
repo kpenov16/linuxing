1a953ebf240237cbd89a4491c4c7e7a4981d031f
kpenov16 <s133967@student.dtu.dk>
2020-03-10 16:28:30 +0100

    - clean up a bit

--- my-apt.sh
+++ my-apt.sh
@@ -67,6 +67,53 @@ if [[ "$source" == *"s"* ]]; then
     fi
   }
  
+  handle_errors 2
+	
+  ##do make
+  echo executing make in: $unzip_path
+  cd $unzip_path && sudo make 
+ 
+  ##do checkinstall
+  echo executing checkinstall in: $unzip_path
+  cd $unzip_path && sudo checkinstall -y 
+
+
+#we have a .deb file to install  
+else
+  pd=$(cd $inst_path; echo "" > pd_tmp && dpkg-deb -I nmap_7.70+dfsg1-6_i386.deb > pd_tmp && awk -F'Depends:' '{print $2}' pd_tmp)
+  
+
+
+fi
+
+#Done. The new package has been installed and saved to
+# /usr/local/src/nmap-7.80/nmap_7.80-1_amd64.deb
+#
+# You can remove it from your system anytime using: 
+#
+#      dpkg -r nmap
+
+
+
+# awk -F'error:' '{print $2}' er.txt
+# install: sudo apt-get install apt-file
+# create local database: sudo apt-file update
+# search for file: apt-file search <filename>
+# sudo apt-get --purge remove flex
+# apt-cache depends package-name 
+
+# https://unix.stackexchange.com/questions/124462/detecting-pattern-at-the-end-of-a-line-with-grep
+# https://stackoverflow.com/questions/12137431/test-if-a-command-outputs-an-empty-string
+# https://unix.stackexchange.com/questions/3514/how-to-grep-standard-error-stream-stderr
+# https://www.cyberciti.biz/faq/linux-list-just-directories-or-directory-names/
+# https://how-to.fandom.com/wiki/How_to_untar_a_tar_file_or_gzip-bz2_tar_file
+# https://stackoverflow.com/questions/3162385/how-to-split-a-string-in-shell-and-get-the-last-field
+# https://www.pluralsight.com/blog/it-ops/linux-file-permissions
+# https://linuxize.com/post/how-to-check-if-string-contains-substring-in-bash/
+# https://stackoverflow.com/questions/428109/extract-substring-in-bash
+
+
+#old version
   handle_errors2(){
     echo running handle_errors2 
     if (( $1 <= 1 )); then
@@ -97,17 +144,7 @@ if [[ "$source" == *"s"* ]]; then
     fi
   }
 
-  handle_errors 2
-	
-  ##do make
-  echo executing make in: $unzip_path
-  cd $unzip_path && sudo make 
- 
-  ##do checkinstall
-  echo executing checkinstall in: $unzip_path
-  cd $unzip_path && sudo checkinstall -y 
-  
-
+#old stuff
 
 #  er=$(cd $unzip_path; echo "" > er_tmp; echo "" > er_fin; ./configure >/dev/null 2> er_tmp; cat er_tmp | awk -F'error:' '{print $2}' > er_fin; cat er_fin  )
 #  if [[ $er ]]; then 
@@ -131,30 +168,5 @@ if [[ "$source" == *"s"* ]]; then
 #  fi
 
 
-fi
 
-#Done. The new package has been installed and saved to
-# /usr/local/src/nmap-7.80/nmap_7.80-1_amd64.deb
-#
-# You can remove it from your system anytime using: 
-#
-#      dpkg -r nmap
-
-
-
-# awk -F'error:' '{print $2}' er.txt
-# install: sudo apt-get install apt-file
-# create local database: sudo apt-file update
-# search for file: apt-file search <filename>
-# sudo apt-get --purge remove flex
-# apt-cache depends package-name 
-
-# https://stackoverflow.com/questions/12137431/test-if-a-command-outputs-an-empty-string
-# https://unix.stackexchange.com/questions/3514/how-to-grep-standard-error-stream-stderr
-# https://www.cyberciti.biz/faq/linux-list-just-directories-or-directory-names/
-# https://how-to.fandom.com/wiki/How_to_untar_a_tar_file_or_gzip-bz2_tar_file
-# https://stackoverflow.com/questions/3162385/how-to-split-a-string-in-shell-and-get-the-last-field
-# https://www.pluralsight.com/blog/it-ops/linux-file-permissions
-# https://linuxize.com/post/how-to-check-if-string-contains-substring-in-bash/
-# https://stackoverflow.com/questions/428109/extract-substring-in-bash
 
